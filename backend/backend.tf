14ed1bd468d38e9a280625a19ef69841cac7874d
pranav yadav <yadavpranav858@gmail.com>
2025-12-18 22:55:11 +0530

    firstly

--- backend/backend.tf
+++ backend/backend.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "s3_remote_backend" {
-    bucket = "devops-automation-project-demo-111223344-utkarsh"
+    bucket = "devops-automation-project-11122334-pranav"
 
     lifecycle {
         prevent_destroy = false
@@ -37,7 +37,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
 }
 
 resource "aws_dynamodb_table" "dynamodb_lock_table" {
-    name = "devops-automation-project-demo-lock-table-11122334-utkarsh"
+    name = "pranav"
     billing_mode = "PAY_PER_REQUEST"
     hash_key = "LockID"
 
